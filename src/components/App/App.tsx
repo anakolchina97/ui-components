6359b5049b73e63278b671f6771cefced2fdc0b8
Annie <anniehartmann97@gmail.com>
2022-06-18 14:22:34 +0300

    More fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,21 @@
 import Layout from 'components/Layout';
-import React from 'react';
+import React, { MouseEvent } from 'react';
+import { Button } from 'ui-kit';
 import styles from './App.module.scss';
 
 type Props = {};
 
 export const App: React.FC = (props: Props) => {
+  const handleButtonClick = (event: MouseEvent) => {
+    console.log('[button click event]', event);
+  };
   return (
     <div className={styles.app}>
       <Layout>
         <h1>React TypeScript UI Library</h1>
+        <hr />
+        <h2>Button</h2>
+        <Button onClick={handleButtonClick}>Кнопка</Button>
       </Layout>
     </div>
   );
